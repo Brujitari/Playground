18133ba339abfd872211963d2e118d589af7dcd0
Brujitari <natimontesg2@mail.com>
2022-08-10 02:55:47 +0200

    My first class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
+    static class Coche {
+        public int puertas = 3;
+        public void IncPuertas() {
+            this.puertas++;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Coche miCoche = new Coche();
+        miCoche.IncPuertas();
+        System.out.println(miCoche.puertas);
     }
 }
